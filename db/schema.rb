0d61789e8b3e9f3303039f3ccf0e86d95fede0dc
Matt Ruttan <matthew.ruttan5@gmail.com>
2016-07-20 19:34:55 +0000

    Generating User Model (has_secure_password) + validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720025143) do
+ActiveRecord::Schema.define(version: 20160720192509) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20160720025143) do
     t.datetime "release_date"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
