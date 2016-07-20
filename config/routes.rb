dd5dfbca0e8ccdb934a25539e7eda3e71e4fd605
Matt Ruttan <matthew.ruttan5@gmail.com>
2016-07-20 19:46:26 +0000

    Adding Links to Log In, Sign Up, or Log Out

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create]
-  
+
+  resources :sessions, only: [:new, :create, :destroy]
+    
 end
