cc1d5d07de1b959d5c373a69cebb7c2a6a3db46d
Matt Ruttan <matthew.ruttan5@gmail.com>
2016-07-20 19:40:50 +0000

    Adding Log In Functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@
         @user = User.new(user_params)
 
         if @user.save
+          session[:user_id] = @user.id # auto log in
           redirect_to movies_path
         else
           render :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
+  get 'sessions/create'
+
   get 'users/new'
 
   get 'users/create'
@@ -8,4 +12,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+  resources :sessions, only: [:new, :create]
+  
 end
